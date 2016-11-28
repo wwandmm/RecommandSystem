f3fd45bfe41bc02de9e8a6a1cd8f9c00452fdb8f
zsl <zsl_hit@qq.com>
2016-11-28 21:24:43 +0800

    just for test

---
+++ java/src/recommand/test.java
@@ -0,0 +1,11 @@
+package recommand;
+
+/**
+ * Created by zsl on 16-11-28.
+ */
+public class test {
+    public static void main(String[] arg)
+    {
+        System.out.print("mm love ww");
+    }
+}
